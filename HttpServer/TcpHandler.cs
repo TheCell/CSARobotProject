5835571b922d3b502af510fc4770d976b8c73884
Kevin Huber <kevin.huber@hotmail.com>
2017-12-19 19:38:33 +0100

    Fixed tcphandler

--- HttpServer/TcpHandler.cs
+++ HttpServer/TcpHandler.cs
@@ -4,64 +4,64 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace HttpServer
 {
     public class TcpHandler
     {
         private readonly TcpClient client;
-        private readonly string fileBuffer;
+        private string fileBuffer;
 
         public TcpHandler(TcpClient client)
         {
             this.client = client;
-            if (File.Exists(@"Temp\positionsLog.csv"))
-            {
-                var streamReader = File.OpenText(@"Temp\positionsLog.csv");
-                this.fileBuffer = streamReader.ReadToEnd();
-            }
         }
 
         public void SendLog()
         {
-			Console.Write("Starting to send Log");
+            if (File.Exists(@"Temp\positionsLog.csv"))
+            {
+                var streamReader = new StreamReader(@"Temp\positionsLog.csv");
+                this.fileBuffer = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+            else
+            {
+                Console.WriteLine("Failed to read file.");
+            }
 
-			if (this.fileBuffer == null)
+            //Console.WriteLine(this.fileBuffer);
+
+            Console.WriteLine("Starting to send Log");
+
+            if (this.fileBuffer == null)
             {
                 return;
             }
-			try
-			{
-				using (NetworkStream networkStream = this.client.GetStream())
-				{
-					using (StreamWriter streamWriter = new StreamWriter(networkStream))
-					{
-						streamWriter.WriteLine("HTTP/1.1 200 OK");
-						streamWriter.WriteLine("Content-Length: " + this.fileBuffer.Length);
-						streamWriter.WriteLine("Content-Type: application/force-download");
-						streamWriter.WriteLine("Content-Transfer-Encoding: binary");
-						streamWriter.WriteLine("Content-Disposition: attachment; filename=\"positionsLog.csv\"");
-						streamWriter.WriteLine();
-						streamWriter.Write(this.fileBuffer);
-						Console.Write(this.fileBuffer);
-						streamWriter.Flush();
-						//streamWriter.Close();
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
-			finally
-			{
-				client.Close();
-			}
+            var streamWriter = new StreamWriter(this.client.GetStream());
+            try
+            {
+                streamWriter.WriteLine("HTTP/1.1 200 OK");
+                streamWriter.WriteLine("Content-Length: " + this.fileBuffer.Length);
+                streamWriter.WriteLine("Content-Type: text/plain");
+                streamWriter.WriteLine();
+                streamWriter.Write(this.fileBuffer);
+                streamWriter.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
 
-			Console.Write("Finished send Log");
+            Console.Write("Finished send Log");
 
-			/*
-			var streamWriter = new StreamWriter(this.client.GetStream());
+            /*
+            var streamWriter = new StreamWriter(this.client.GetStream());
             streamWriter.WriteLine("HTTP/1.1 200 OK");
             streamWriter.WriteLine("Content-Length: " + this.fileBuffer.Length);
             streamWriter.WriteLine("Content-Type: application/force-download");
@@ -72,7 +72,7 @@ namespace HttpServer
             streamWriter.Flush();
             streamWriter.Close();
             client.Close();
-			*/
-		}
-	}
+            */
+        }
+    }
 }
